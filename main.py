50ecd7df890ed7e67ce3523f0ba02ba75a45db56
Крас Михаил <krasmihael@gmail.com>
2022-09-28 12:14:18 +0300

    backend done

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+import socket
+import multiprocessing
+import time
+
+
+def gen():
+    m = 0
+    while True:
+        m += 1
+        yield m
+
+
+def create_random_number(q, e):
+    while True:
+        random_number = random.randint(0, 100)
+        print(random_number)
+        q.put(random_number)
+        e.set()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    random.seed()
+    e = multiprocessing.Event()
+    q = multiprocessing.Queue()
+    w1 = multiprocessing.Process(name='random_number_generator',
+                                 target=create_random_number,
+                                 args=(q,e,))
+    w1.start()
+    serverSocket = socket.socket()
+    ip = "127.0.0.1"
+    port = 35491
+    serverSocket.bind((ip, port))
+    (clientConnection, clientAddress) = serverSocket.accept()
+    for _ in gen():
+        e.wait()
+        serverSocket.sendall(str.encode(str(q.get())))
